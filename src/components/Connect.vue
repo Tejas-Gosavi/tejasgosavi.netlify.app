9837102f7153c91146c158e8eafa942babb70ed2
Tejas-Gosavi <tejasgosavi555@gmail.com>
2022-07-10 00:14:47 +0530

    fixed svg bug

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -4,19 +4,19 @@
 			Connect with me on
 			<div class="d-flex flex-wrap justify-content-start align-items-center">
 				<a target="_blank" href="mailto:tejasgosavi555@gmail.com">
-					<img src="src/assets/logo/gmail.svg" alt="Gmail" class="svg-img"
+					<img src="../assets/logo/gmail.svg" alt="Gmail" class="svg-img"
 					/>
 				</a>,
 				<a target="_blank" href="https://www.linkedin.com/in/tejasgosavi555/">
-					<img src="src/assets/logo/linkedin.svg" alt="LinkedIn" class="svg-img"
+					<img src="../assets/logo/linkedin.svg" alt="LinkedIn" class="svg-img"
 					/>
 				</a>,
 				<a target="_blank" href="https://github.com/Tejas-Gosavi">
-					<img src="src/assets/logo/github.svg" alt="GitHub" class="svg-img"
+					<img src="../assets/logo/github.svg" alt="GitHub" class="svg-img"
 					/>
 				</a>,
 				<a target="_blank" href="https://stackoverflow.com/users/17355953/tejas-gosavi">
-					<img src="src/assets/logo/stackoverflow.svg" alt="Stackoverflow" class="svg-img"/>
+					<img src="../assets/logo/stackoverflow.svg" alt="Stackoverflow" class="svg-img"/>
 				</a>.
 			</div><br>
 			<a class="special-link" target="_blank" href="https://drive.google.com/file/d/1i-hhxBzYsrNvFFi7CoYrAGTRA-MVizZZ/view?usp=sharing">
