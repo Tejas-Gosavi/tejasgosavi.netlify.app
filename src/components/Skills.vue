04364d36914ef659bbd928ad08d012e79308a398
Tejas-Gosavi <tejasgosavi555@gmail.com>
2022-07-11 13:31:13 +0530

    code improvements

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,25 +1,23 @@
 <template>
-	<section class="container mt-3">
-		<div class="mt-5 hero-text">
-			I mostly works in
-			<div class="d-flex flex-wrap justify-content-start align-items-center">
-				<img src="../assets/logo/c++.svg" alt="C++" class="svg-img" />,
-				<img src="../assets/logo/python.svg" alt="Python" class="svg-img" />,
-				<img src="../assets/logo/go.svg" alt="Golang" class="svg-img" />,
-				<img src="../assets/logo/js.svg" alt="JavaScript" class="svg-img" />.
-			</div><br>
-			I have decent knowledge of
-			<div class="d-flex flex-wrap justify-content-start align-items-center">
-				<img src="../assets/logo/django.svg" alt="Django" class="svg-img" />,
-				<img src="../assets/logo/vuejs.svg" alt="Vue.js" class="svg-img" />,
-				<img src="../assets/logo/angular.svg" alt="Angular" class="svg-img" />,
-				<img src="../assets/logo/nodejs.svg" alt="Node.js" class="svg-img" />,
-				<img src="../assets/logo/mysql.svg" alt="MySQL" class="svg-img" />,
-				<img src="../assets/logo/mongodb.svg" alt="MongoDB" class="svg-img" />, 
-				<img src="../assets/logo/bootstrap.svg" alt="Bootstrap" class="svg-img" />.
-			</div><br>
-			I also have interest in Machine learning, Robotics.
-		</div>
+	<section class="container mt-5 hero-text">
+		I mostly works in
+		<div class="d-flex flex-wrap justify-content-start align-items-center">
+			<img src="../assets/logo/c++.svg" alt="C++" class="svg-img" />,
+			<img src="../assets/logo/python.svg" alt="Python" class="svg-img" />,
+			<img src="../assets/logo/go.svg" alt="Golang" class="svg-img" />,
+			<img src="../assets/logo/js.svg" alt="JavaScript" class="svg-img" />.
+		</div><br>
+		I have decent knowledge of
+		<div class="d-flex flex-wrap justify-content-start align-items-center">
+			<img src="../assets/logo/django.svg" alt="Django" class="svg-img" />,
+			<img src="../assets/logo/vuejs.svg" alt="Vue.js" class="svg-img" />,
+			<img src="../assets/logo/angular.svg" alt="Angular" class="svg-img" />,
+			<img src="../assets/logo/nodejs.svg" alt="Node.js" class="svg-img" />,
+			<img src="../assets/logo/mysql.svg" alt="MySQL" class="svg-img" />,
+			<img src="../assets/logo/mongodb.svg" alt="MongoDB" class="svg-img" />, 
+			<img src="../assets/logo/bootstrap.svg" alt="Bootstrap" class="svg-img" />.
+		</div><br>
+		I also have interest in Machine learning, Robotics.
 	</section>
 </template>
 
